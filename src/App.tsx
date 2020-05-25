6e11756aec34ac321f74348057830bcdb2ce605b
Kirill Khramkov <hramkir@gmail.com>
2020-05-25 12:05:36 +0300

    Updated about page

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ const App: React.FC = () => {
                     <Route component={AboutPage} path="/about" exact/>
                 </Switch>
             </div>
+
         </BrowserRouter>
     )
 }
--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import {useHistory} from 'react-router-dom'
+import GithubLogo from './github-logo.svg'
 
 const AboutPage: React.FC = () => {
     const history = useHistory()
     return (
         <React.Fragment>
             <h1>About</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deserunt dicta facere porro quasi
-                sapiente
-                similique! Adipisci, aperiam, assumenda atque dolorem ea error ipsa iste, itaque magni minus nobis omnis
-                porro quae repellendus temporibus tenetur velit veritatis vitae voluptatem voluptatum!</p>
+            <p>
+                Simple Full-stack To Do React App.
+            </p>
+            <a href="https://github.com/Discretus/react-todo-app">
+                <div className="mt-4 github">
+                    Go to Github repo
+                    &nbsp;
+                    <img src={GithubLogo} alt="GutHib logo"/>
+                </div>
+            </a>
             <button className="waves-effect waves-light btn-large grey darken-3 mt-4"
                     onClick={() => history.push('/')}>
                 Go back
